1de1ed950479f533decf08576638c38ea09ebf01
Craig Damlo <craig.damlo@soapboxrocket.com>
2022-07-21 20:30:23 -0700

    script10

---
+++ c10.py
@@ -0,0 +1,6 @@
+# Initialize the list
+weekdays = ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday","Friday", "Saturday"] 
+print("The Seven Days of the Week are:")
+# Iterate the list using for loop
+for day in range(len(weekdays)): 
+    print(weekdays[day])
