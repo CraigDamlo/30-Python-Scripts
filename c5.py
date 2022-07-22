73534058c75cc978eb89006d99cc58145764786e
Craig Damlo <craig.damlo@soapboxrocket.com>
2022-07-21 18:48:57 -0700

    script5

---
+++ c5.py
@@ -0,0 +1,19 @@
+# Boolean value
+val1 = True
+print(val1)
+
+# Number to Boolean
+number = 10
+print(bool(number))
+
+number = -5
+print(bool(number))
+
+number = 0
+print(bool(number))
+
+# Boolean from comparison operator
+val2 = 6
+val3 = 3
+print(val3 < val2)
+print(val2 < val3)
