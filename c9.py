f1e2655e748aea5e45a8365ee3109a14cb0e5b56
Craig Damlo <craig.damlo@soapboxrocket.com>
2022-07-21 20:26:01 -0700

    script9

---
+++ c9.py
@@ -0,0 +1,9 @@
+# Initialize counter
+counter = 1
+loops = float(input("How many loops?: "))
+# Iterate the loop 5 times
+while counter <= loops:
+    # Print the counter value
+    print ("The current counter value: %d" % counter)
+    # Increment the counter
+    counter = counter + 1
