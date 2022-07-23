38128d698b53e43bea00baa0b72303026839d38d
Craig Damlo <craig.damlo@soapboxrocket.com>
2022-07-22 18:57:50 -0700

    script18

---
+++ c18.py
@@ -0,0 +1,14 @@
+# Assign string value
+text = "Learn Python P-r-o-g-r-a-m-m-i-n-g"
+
+# Slice using one parameter
+sliceObj = slice(5) 
+print(text[sliceObj])  
+
+# Slice using two parameter
+sliceObj = slice(6,12) 
+print(text[sliceObj])  
+
+# Slice using three parameter
+sliceObj = slice(13,36,2) 
+print(text[sliceObj])
