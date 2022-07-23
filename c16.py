1be626554361833246e220ad838d813fa7f68cbc
Craig Damlo <craig.damlo@soapboxrocket.com>
2022-07-22 18:46:54 -0700

    script16

---
+++ c16.py
@@ -0,0 +1,20 @@
+# Declare a fruit list
+fruits = ["Mango","Orange","Guava","Banana"]
+
+# Displaying list of fruit
+print("The fruit list:")
+print(fruits)
+
+# Insert an item in the 2nd position
+fruits.insert(1, "Grape")
+
+# Displaying list after inserting
+print("The fruit list after insert:")
+print(fruits)
+  
+# Remove an item
+fruits.remove("Guava")
+
+# Print the list after delete
+print("The fruit list after delete:")
+print(fruits)
